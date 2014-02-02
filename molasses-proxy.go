15bb962a18f4d8634df56ac2af7ff433f136f6bf
ArtemTitoulenko <artem.titoulenko@gmail.com>
2014-02-02 02:06:39 -0500

    added linkio library, molasses now limits speed

--- molasses-proxy.go
+++ molasses-proxy.go
@@ -1,62 +1,40 @@
 package main
 
 import (
-  "github.com/elazarl/goproxy"
-  "log"
-  "net/http"
-  "time"
-  "os"
-  "io/ioutil"
-  "strings"
-  "flag"
-  "fmt"
+	"flag"
+	"fmt"
+	"github.com/elazarl/goproxy"
+	"github.com/ox/molasses-proxy/linkio"
+	"log"
+	"net/http"
 )
 
-var blocked_filename = "./blocked_hosts"
-var request_number = 0
-var port = flag.Int("port", 8080, "the port to listen for requests on")
-var delay = flag.Int("delayms", 500, "increase the delay by this many milliseconds per request")
-var help = flag.Bool("help", false, "print this help message")
-
-func slowBan(r *http.Request, cst *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-  log.Println("banned request for", r.URL.String(), ", waiting:",request_number * (*delay),"milliseconds");
-  time.Sleep(time.Duration(request_number * (*delay)))
-  request_number += 1
-  return r, nil
-}
+var port = flag.Int("port", 8080,
+	"the port to listen for requests on")
+var help = flag.Bool("help", false,
+	"print this help message")
+var rate = flag.Int("rate", 56,
+	"set the maximum link rate in kbps")
 
 func main() {
-  flag.Parse()
+	flag.Parse()
+	slowLink := linkio.NewLink(*rate)
 
-  if *help {
-    fmt.Println("usage:\n\n\tmolasses-proxy [--port=8080] [--delayms=500]\n")
-    flag.PrintDefaults()
-    return
-  }
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = false
 
+	proxy.OnResponse().DoFunc(
+		func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			resp.Body = slowLink.NewLinkReader(resp.Body)
+			return resp
+		})
 
-  proxy := goproxy.NewProxyHttpServer()
-  proxy.Verbose = false
+	if *help {
+		fmt.Println("usage:\n\n\tmolasses-proxy [--port=8080] [--rate=0]\n")
+		flag.PrintDefaults()
+		return
+	}
 
-  if _, err := os.Stat(blocked_filename); os.IsNotExist(err) {
-    log.Fatalf("no such file or directory: %s\n", blocked_filename)
-  }
-
-  data, err := ioutil.ReadFile(blocked_filename)
-  if err != nil {
-    log.Fatalf("error reading %s: %s\n", blocked_filename, err)
-  }
-
-  websites := strings.Split(string(data), "\n")
-  for _, website := range websites {
-    if website == "" {
-      break
-    }
-
-    log.Println("slow banning", website)
-    proxy.OnRequest(goproxy.DstHostIs(website)).DoFunc(slowBan)
-  }
-
-  log.Println("starting proxy on :", *port, "delaying", *delay, "more milliseconds per request")
-  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), proxy))
+	log.Println("starting proxy on :", *port, "@", *rate, "kbps")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), proxy))
 }
